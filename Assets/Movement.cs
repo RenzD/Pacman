41022be2cde694326b20368b2144c7151dbfa3bc
RenzD <renzdionglay@gmail.com>
2019-11-30 02:58:02 -0800

    Added 2nd scene for GA, finished Selection Phase

---
+++ Assets/Movement.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    static int pacnums = 200;
+    public int[] movement = new int[pacnums];
+
+    bool movingUP = false;
+    bool movingDOWN = false;
+    bool movingLEFT = false;
+    bool movingRIGHT = false;
+    public bool movesDone = false;
+
+    public int[,] path2D;
+    private const int ROWS = 29;
+    private const int COLS = 26;
+
+    int index = 0;
+    bool once = false;
+    float pacman_move_row = 0;
+    float pacman_move_col = 0;
+    float pacman_current_row = 0;
+    float pacman_current_col = 0;
+    float pacman_speed = 8.0f;
+
+    public double fitness = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        path2D = new int[ROWS, COLS] {
+            { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 },
+            { 1,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1 },
+            { 1,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1 },
+            { 1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1 },
+            { 0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0 },
+            { 0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0 },
+            { 1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1 },
+            { 1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1 },
+            { 1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1 },
+            { 1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1 },
+            { 0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,0,0,1,0,0,0,1,1,0,0,0,1,0,0,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,0,0,1,0,0,0,1,1,0,0,0,1,0,0,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0 },
+            { 1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1 },
+            { 1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1 },
+            { 1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1 },
+            { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 },
+            { 1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1 },
+            { 1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1 },
+            { 1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1 },
+            { 1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1 }
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePacman();
+        Fitness();
+    }
+
+    private void MovePacman()
+    {
+        if (index < pacnums && !movesDone)
+        {
+            if (movement[index] == 1 && !movingRIGHT && !movingDOWN && !movingLEFT)
+            {
+                movingUP = true;
+                transform.localScale = new Vector3(0.8f, 0.8f, 1);
+                transform.localRotation = Quaternion.Euler(0, 0, 90);
+            }
+            else if (movement[index] == 2 && !movingLEFT && !movingDOWN && !movingUP)
+            {
+                movingRIGHT = true;
+                transform.localScale = new Vector3(0.8f, 0.8f, 1);
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+            else if (movement[index] == 3 && !movingRIGHT && !movingLEFT && !movingUP)
+            {
+                movingDOWN = true;
+                transform.localScale = new Vector3(0.8f, 0.8f, 1);
+                transform.localRotation = Quaternion.Euler(0, 0, 270);
+            }
+            else if (movement[index] == 4 && !movingRIGHT && !movingDOWN && !movingUP)
+            {
+                movingLEFT = true;
+                transform.localScale = new Vector3(-0.8f, 0.8f, 1);
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+
+            if (movingUP)
+            {
+                int pacman_up = (int)pacman_current_row + 1;
+                if (pacman_up < 29 && path2D[pacman_up, (int)pacman_current_col] != 0)
+                {
+                    pacman_move_row += pacman_speed * Time.deltaTime;
+                    transform.position = new Vector3(pacman_current_col, pacman_move_row, 0.0f);
+
+                    if (pacman_move_row > pacman_up)
+                    {
+                        transform.position = new Vector3(pacman_current_col, pacman_up, 0.0f);
+                        pacman_current_row = pacman_up;
+                        pacman_move_row = pacman_up;
+                        movingUP = false;
+                    }
+                }
+                else
+                {
+                    movingUP = false;
+                }
+            } 
+            else if (movingRIGHT)
+            {
+                float pacman_right = (int)pacman_current_col + 1;
+                if (pacman_right < 26 && path2D[(int)pacman_current_row, (int)pacman_right] != 0)
+                {
+                    pacman_move_col += pacman_speed * Time.deltaTime;
+                    transform.position = new Vector3(pacman_move_col, pacman_current_row, 0.0f);
+
+                    if (pacman_move_col > pacman_right)
+                    {
+                        transform.position = new Vector3(pacman_right, pacman_current_row, 0.0f);
+                        pacman_current_col = pacman_right;
+                        pacman_move_col = pacman_right;
+                        movingRIGHT = false;
+                    }
+                }
+                else
+                {
+                    movingRIGHT = false;
+                }
+            } 
+            else if (movingDOWN)
+            {
+                float pacman_down = (int)pacman_current_row - 1;
+                if (pacman_down >= 0 && path2D[(int)pacman_down, (int)pacman_current_col] != 0)
+                {
+                    pacman_move_row -= pacman_speed * Time.deltaTime;
+                    transform.position = new Vector3(pacman_current_col, pacman_move_row, 0.0f);
+
+
+                    if (pacman_move_row < pacman_down)
+                    {
+                        transform.position = new Vector3(pacman_current_col, pacman_down, 0.0f);
+                        pacman_current_row = pacman_down;
+                        pacman_move_row = pacman_down;
+                        movingDOWN = false;
+                    }
+                }
+                else
+                {
+                    movingDOWN = false;
+                }
+            } 
+            else if (movingLEFT)
+            {
+                float pacman_left = (int)pacman_current_col - 1;
+                if (pacman_left >= 0 && path2D[(int)pacman_current_row, (int)pacman_left] != 0)
+                {
+
+                    pacman_move_col -= pacman_speed * Time.deltaTime;
+                    transform.position = new Vector3(pacman_move_col, pacman_current_row, 0.0f);
+
+
+                    if (pacman_move_col < pacman_left)
+                    {
+                        transform.position = new Vector3(pacman_left, pacman_current_row, 0.0f);
+                        pacman_current_col = pacman_left;
+                        pacman_move_col = pacman_left;
+                        movingLEFT = false;
+                    }
+                }
+                else
+                {
+                    movingLEFT = false;
+                }
+            }
+
+            if (!movingUP && !movingRIGHT && !movingDOWN && !movingLEFT)
+            {
+                index++;
+                if (index >= movement.Length)
+                {
+                    movesDone = true;
+                }
+            }
+        }
+    }
+
+    public void Fitness()
+    {
+        if (movesDone && !once)
+        {
+            once = true;
+            fitness = ((ROWS - 1) - pacman_current_row) + ((COLS - 1) - pacman_current_col);
+            fitness = Math.Round(Math.Sqrt(fitness), 5);
+            //Debug.Log("Fitness: " + fitness);
+        }
+    }
+
+    public void SetMoves(int[] moves)
+    {
+        movement = moves;
+    }
+
+    public int[] GetMoveArray()
+    {
+        return movement;
+    }
+}
---
+++ Assets/Scripts/Genetic.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Genetic : MonoBehaviour
+{
+    public GameObject pacmanPrefab;
+    private const int ROWS = 29;
+    private const int COLS = 26;
+
+    static int pacnums = 8;
+    static int parents_num = 2;
+    static int movenums = 200;
+    
+    public int[,] path2D;
+    public int[] movement;
+    public int[] children_movement;
+    public int[] parent_movement;
+    Movement[] moveScript;
+    GameObject[] pacman;
+
+    bool once = false;
+    bool generationDone = false;
+    double[] fitness;
+
+    SortedSet<FitnessScore> openList;
+
+    class FitnessComparer : IComparer<FitnessScore>
+    {
+        public int Compare(FitnessScore x, FitnessScore y)
+        {
+            //first by age
+            int result = x.Fitness.CompareTo(y.Fitness);
+
+            return result;
+        }
+    }
+
+    class FitnessScore
+    {
+        public double Fitness { get; set; }
+
+        public int[] MoveArray { get; set; }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        path2D = new int[ROWS, COLS] {
+            { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 },
+            { 1,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1 },
+            { 1,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1 },
+            { 1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1 },
+            { 0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0 },
+            { 0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0 },
+            { 1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1 },
+            { 1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1 },
+            { 1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1 },
+            { 1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1 },
+            { 0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,0,0,1,0,0,0,1,1,0,0,0,1,0,0,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,0,0,1,0,0,0,1,1,0,0,0,1,0,0,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0 },
+            { 0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0 },
+            { 1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1 },
+            { 1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1 },
+            { 1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1 },
+            { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 },
+            { 1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1 },
+            { 1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1 },
+            { 1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1 },
+            { 1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1 }
+        };
+
+        
+        int move;
+        pacman = new GameObject[pacnums];
+        moveScript = new Movement[pacnums];
+        for (int i = 0; i < pacnums; i++)
+        {
+            pacman[i] = Instantiate(pacmanPrefab, transform.position, transform.rotation);
+            moveScript[i] = pacman[i].GetComponent<Movement>();
+            movement = new int[movenums];
+
+            for (int j = 0; j < movenums; j++)
+            {
+                move = UnityEngine.Random.Range(1, 5);
+                movement[j] = move;
+            }
+            moveScript[i].SetMoves(movement);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!generationDone)
+        {
+            int doneCounter = 0;
+            for (int i = 0; i < pacnums; i++)
+            {
+                if (moveScript[i].movesDone)
+                {
+                    doneCounter++;
+                }
+            }
+            if (doneCounter == pacnums)
+            {
+                generationDone = true; 
+                Debug.Log("generationDone");
+                fitness = new double[pacnums];
+                for (int i = 0; i < pacnums; i++)
+                {
+                    fitness[i] = moveScript[i].fitness;
+                    //Debug.Log("Fitness " + i + ": " + fitness[i]);
+                }
+            }
+        } else
+        {
+            if (!once)
+            {
+                //DO ONLY ONCE
+                once = true;
+                Selection();
+                Crossover();
+                Mutation();
+            }
+            
+        }
+    }
+
+    private void Selection()
+    {
+        openList = new SortedSet<FitnessScore>(new FitnessComparer());
+        FitnessScore fn;
+        for (int i = 0; i < pacnums; i++)
+        {
+            fn = new FitnessScore();
+            fn.Fitness = fitness[i];
+            fn.MoveArray = moveScript[i].GetMoveArray();
+            string str = "";
+            foreach (int x in fn.MoveArray) {
+                str += x.ToString();
+            }
+            Debug.Log("Fitness " + i + ": " + fitness[i]);
+            Debug.Log(str);
+            openList.Add(fn);
+        }
+        Debug.Log("\n");
+
+        int parentCounter = 2;
+        parent_movement = new int[movenums];
+        while (openList.Count != 0)
+        {
+            FitnessScore p = openList.First();
+            openList.Remove(p);
+
+            if (parentCounter > 0)
+            {
+                parent_movement = p.MoveArray;
+                Debug.Log("Fitness: " + p.Fitness);
+                string str = "";
+                foreach (int x in p.MoveArray)
+                {
+                    str += x.ToString();
+                }
+                Debug.Log("Array: " + str);
+                parentCounter--;
+            }
+        }
+    }
+
+    private void Crossover()
+    {
+        //Mix parents movement[] to create 8 variety of movement[] 
+        //1/2
+        //1/2 reverse
+        //1/4
+        //1/4 reverse
+        //1/8
+        //1/8 reverse
+        //keep top 2 from the previous gen
+    }
+
+    private void Mutation()
+    {
+
+    }
+}
